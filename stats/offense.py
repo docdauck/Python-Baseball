dc320071749cfc09f229cbb54cef5331685b5db4
Tim-Frederik Dauck <tfd23@cam.ac.uk>
2020-06-23 14:04:57 +0100

    completed module 5

--- stats/offense.py
+++ stats/offense.py
@@ -6,7 +6,7 @@ from data import games
 plays = games[games['type'] == 'play']
 plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
 
-hits = plays.loc[plays['event'].str.contains(r'^(?:S(?!B)|D|HR)'), ['inning', 'event']]
+hits = plays.loc[plays['event'].str.contains('^(?:S(?!B)|D|HR)'), ['inning', 'event']]
 hits.loc[:, 'inning'] = pd.to_numeric(hits.loc[:, 'inning'])
 
 replacements = {r'^S(.*)': 'single', r'^D(.*)': 'double', r'^T(.*)': 'triple', r'^HR(.*)': 'hr'}
