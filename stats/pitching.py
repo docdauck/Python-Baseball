bad847f553689497d0465dfab225d11f71b480e8
Tim-Frederik Dauck <tfd23@cam.ac.uk>
2020-06-23 13:35:38 +0100

    attempt at 2.4

--- stats/pitching.py
+++ stats/pitching.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from data import games
 
 
-plays = games[games['type'] == 'play', 'plays']
+plays = games[games['type'] == 'play']
 
 strike_outs = plays[plays['event'].str.contains('K')]
 
@@ -12,8 +12,8 @@ strike_outs = strike_outs.groupby(['year', 'game_id']).size()
 
 strike_outs = strike_outs.reset_index(name='strike_outs')
 
-strike_outs = strike_outs.loc[:, ['year', 'strike_out']].apply(pd.to_numeric)
+#strike_outs = strike_outs.loc[:, ['year', 'strike_out']].apply(pd.to_numeric)
 
-strike_outs.plot(x='year', y='strike_outs', kind='scatter').legend(['Strike Outs'])
+#strike_outs.plot(x='year', y='strike_outs', kind='scatter').legend(['Strike Outs'])
 
-plt.show()
+#plt.show()
